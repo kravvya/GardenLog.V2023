4ff8325ecd7f1a629456dbadc8622cf172da0334
Steve Kravchenko <kravvya@chrobinson.com>
2023-01-04 13:50:02 -0600

    added missing using

--- GardenLogWeb/GardenLogWeb/Models/VisualComponent.cs
+++ GardenLogWeb/GardenLogWeb/Models/VisualComponent.cs
@@ -1,11 +1,10 @@
-﻿namespace GardenLogWeb.Models
+﻿namespace GardenLogWeb.Models;
+
+public abstract class VisualComponent
 {
-    public abstract class VisualComponent
-    {
-        public int Length { get; set; }
-        public int Width { get; set; }
-        public double X { get; set; }
-        public double Y { get; set; }
-        public string? BorderColor { get; set; }
-    }
+    public int Length { get; set; }
+    public int Width { get; set; }
+    public double X { get; set; }
+    public double Y { get; set; }
+    public string? BorderColor { get; set; }
 }
