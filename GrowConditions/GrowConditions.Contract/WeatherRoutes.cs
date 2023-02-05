e03504fefd94b0d1c57972dde14e81090fb49f2b
Steve Kravchenko <kravvya@chrobinson.com>
2023-02-05 01:25:34 -0600

    Gorw Conditions

--- GrowConditions/GrowConditions.Contract/WeatherRoutes.cs
+++ GrowConditions/GrowConditions.Contract/WeatherRoutes.cs
@@ -2,7 +2,7 @@
 
 public static class WeatherRoutes
 {
-    public const string WeatherBase = "api/v1/weather";
+    public const string WeatherBase = "/v1/api/weather";
 
     public const string GetLastWeatherUpdate = WeatherBase + "/garden/{gardenId}/last";
     public const string GetHistoryOfWeatherUpdates = WeatherBase + "/garden/{gardenId}/history/{numberOfDays}";
