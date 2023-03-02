c5bdd93fba1569e0c0eb37f6ab3f23859fb8b2ca
Steve Kravchenko <kravvya@chrobinson.com>
2023-03-02 17:10:44 -0600

    Changed logic for Harvest schedule to calcualte for all planting methonds.

--- PlantHarvest/PlantHarvest.Api/EventHandlers/Schedules/HarvestScheduler.cs
+++ PlantHarvest/PlantHarvest.Api/EventHandlers/Schedules/HarvestScheduler.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using p = PlantCatalog.Contract.Enum;
 
 
 
@@ -8,22 +8,47 @@ public class HarvestScheduler : SchedulerBase, IScheduler
 {
     public bool CanSchedule(PlantGrowInstructionViewModel growInstruction)
     {
-        return growInstruction.TransplantWeeksAheadOfWeatherCondition.HasValue;
+        return true;
     }
 
     public CreatePlantScheduleCommand? Schedule(PlantHarvestCycle harvestCycle, PlantGrowInstructionViewModel growInstruction, GardenViewModel garden, int? daysToMaturityMin, int? daysToMaturityMax)
     {
-        int weeksAhead = growInstruction.TransplantWeeksAheadOfWeatherCondition.HasValue ? growInstruction.TransplantWeeksAheadOfWeatherCondition.Value : 0;
+        DateTime originDate;
 
-        DateTime? transplantDate = GetStartDateBasedOnWeatherCondition(growInstruction.TransplantAheadOfWeatherCondition, weeksAhead, garden);
+        switch (growInstruction.PlantingMethod)
+        {
+            case p.PlantingMethodEnum.SeedIndoors:
+            case p.PlantingMethodEnum.Transplanting:
+                if (growInstruction.TransplantAheadOfWeatherCondition == p.WeatherConditionEnum.Unspecified) return null;
 
-        if (transplantDate.HasValue && daysToMaturityMin.HasValue && daysToMaturityMax.HasValue)
+                int weeksAhead = growInstruction.TransplantWeeksAheadOfWeatherCondition.HasValue ? growInstruction.TransplantWeeksAheadOfWeatherCondition.Value : 0;
+
+                DateTime? transplantDate = GetStartDateBasedOnWeatherCondition(growInstruction.TransplantAheadOfWeatherCondition, weeksAhead, garden);
+
+                if (transplantDate.HasValue) { originDate = transplantDate.Value; } else return null;
+
+                break;
+            case p.PlantingMethodEnum.DirectSeed:
+                if (growInstruction.StartSeedAheadOfWeatherCondition == p.WeatherConditionEnum.Unspecified) return null;
+
+                weeksAhead = growInstruction.StartSeedWeeksAheadOfWeatherCondition.HasValue ? growInstruction.StartSeedWeeksAheadOfWeatherCondition.Value : 0;
+
+                DateTime? seedDate = GetStartDateBasedOnWeatherCondition(growInstruction.StartSeedAheadOfWeatherCondition, weeksAhead, garden);
+
+                if (seedDate.HasValue) { originDate = seedDate.Value; } else return null;
+
+                break;
+            default:
+                return null;
+        }
+
+        if (daysToMaturityMin.HasValue && daysToMaturityMax.HasValue)
         {
             return new CreatePlantScheduleCommand()
             {
                 TaskType = WorkLogReasonEnum.Harvest,
-                StartDate = transplantDate.Value.AddDays(daysToMaturityMin.Value),
-                EndDate = transplantDate.Value.AddDays(daysToMaturityMax.Value),
+                StartDate = originDate.AddDays(daysToMaturityMin.Value),
+                EndDate = originDate.AddDays(daysToMaturityMax.Value),
                 IsSystemGenerated = true,
                 Notes = growInstruction.HarvestInstructions
             };
