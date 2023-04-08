f82e88d23fe08d5520608f73fac2916098a3ef49
Steve Kravchenko <kravvya@chrobinson.com>
2023-04-07 21:49:47 -0500

    Added logging to UnitOfWork

--- GardenLog.SharedInfrastructure/MongoDB/MongoDBUnitOfWork.cs
+++ GardenLog.SharedInfrastructure/MongoDB/MongoDBUnitOfWork.cs
@@ -1,4 +1,6 @@
-﻿using GardenLog.SharedKernel.Interfaces;
+﻿using DnsClient.Internal;
+using GardenLog.SharedKernel.Interfaces;
+using Microsoft.Extensions.Logging;
 
 namespace GardenLog.SharedInfrastructure.MongoDB;
 
@@ -6,11 +8,13 @@ public class MongoDBUnitOfWork : IUnitOfWork
 {
     private readonly List<Func<Task>> _commands;
     private readonly IMongoDBContext _context;
+    private readonly ILogger<MongoDBUnitOfWork> _logger;
     private string? _rootHandler = null;
 
-    public MongoDBUnitOfWork(IMongoDBContext context)
+    public MongoDBUnitOfWork(IMongoDBContext context, ILogger<MongoDBUnitOfWork> logger)
     {
         _context = context;
+        _logger = logger;
 
         // Every command will be stored and it'll be processed at SaveChanges
         _commands = new List<Func<Task>>();
@@ -37,12 +41,15 @@ public class MongoDBUnitOfWork : IUnitOfWork
         {
             if (string.IsNullOrWhiteSpace(handlerName) || handlerName != _rootHandler)
             {
+                _logger.LogWarning("Request did not originate from root handler. Changes will not be saved.");
                 return 0;
             }
         }
 
         int numberOfChanges = await _context.ApplyChangesAsync(_commands);
         _commands.Clear();
+
+        _logger.LogInformation("Request originate from root handler. {0} changes were saved.", numberOfChanges);
         return numberOfChanges;
     }
 
